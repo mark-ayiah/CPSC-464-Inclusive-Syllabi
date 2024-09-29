754ee298be380e0bd3424288de0c1324e844aa5d
winiboya <w>
2024-09-28 20:43:36 -0400

    interface

---
+++ interface/README.md
@@ -0,0 +1,26 @@
+# Algorithmic Curation of Inclusive Syllabi: 
+## Tool for Measuring and Recommending Diversity in Course Content
+
+
+## Running the Program
+Make sure you're in the proper directory by running the following command from the root directory:
+```
+cd interface
+```
+
+Start a virtual environment by running the following commands:
+```
+python3 -m venv syllabi_env
+source syllabi_env/bin/activate
+```
+
+Install the required packages. You can do this by running the following command:
+```
+pip install -r requirements.txt
+```
+
+Run the program by running the following command:
+```
+python3 app.py
+```
+
---
+++ interface/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # You can pass variables to the template if needed
+    return render_template('index.html', show_results=False)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    isbns = request.form.get('isbns').split(',')
+    print(f"ISBN: {isbns}")
+    return render_template('index.html', diversity=calc_diversity(isbns), suggestions=get_suggestions(isbns), show_results=True, isbns=','.join(isbns))
+
+def calc_diversity(isbns):
+    return 0.349
+
+def get_suggestions(isbns):
+    return ['To Kill a Mocking Bird', 'Autobiography of Malcolm X', '1984' , "Harry Potter"]
+
+if __name__ == '__main__':
+    app.run(debug=True)
