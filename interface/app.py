5f588e2846fab32bfd03e3e2a1b7035e4ed805c1
winiboya <w>
2024-10-01 21:06:03 -0400

    form on home page done

--- interface/app.py
+++ interface/app.py
@@ -4,19 +4,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print("HELLO")
     return render_template('index.html', show_results=False)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     isbns = request.form.get('isbns').split(',')
     print(f"ISBN: {isbns}")
-    return render_template('index.html', diversity=calc_diversity(isbns), suggestions=get_suggestions(isbns), show_results=True, isbns=','.join(isbns))
+    return render_template('validate.html', isbns=isbns)
+
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    
+    
+    return render_template('results.html')
+
+@app.route('/results', methods=['POST'])
+def results(isbns):
+    diversity = calc_diversity(isbns)
+    suggestions = get_suggestions(isbns)
+    return render_template('results.html', diversity=diversity, suggestions=suggestions)
 
 def calc_diversity(isbns):
     return 0.349
 
 def get_suggestions(isbns):
-    return ['To Kill a Mocking Bird', 'Autobiography of Malcolm X', '1984' , "Harry Potter"]
+    return ['Their Eyes Were Watching By God',  'Invisible Man' , 'Native Son']
 
 if __name__ == '__main__':
     app.run(debug=True)
